c11b7b2d5ca34bc11f35a79ae2394edb7cea738a
Mikhail S Medvedev <mmedvede@us.ibm.com>
2015-09-21 18:24:58 -0500

    WIP, do not merge: Add proof of concept puppet-ciwatch module
    Related spec in progress: https://review.openstack.org/#/c/194437/
    
    Change-Id: Iab6f006d42bb131b7894a0e3241447786398093a

---
+++ monitoring/puppet-ciwatch/manifests/initd_service.pp
@@ -0,0 +1,31 @@
+# == Define: ciwatch::initd_service
+#
+# Creates initd service for an executable which can not run as daemon and
+# runs it in the python virtualenv.
+
+define ciwatch::initd_service(
+  $exec_cmd,
+  $short_description,
+  $runas_user,
+  $service_name = $title,
+) {
+
+  # Template uses:
+  # service_name
+  # short_description
+  # runas_user
+  file { "/etc/init.d/${service_name}":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0555',
+    content => template('ciwatch/ciwatch_service.init.erb'),
+  }
+
+  service { $service_name:
+    enable     => true,
+    hasrestart => true,
+    require    => File["/etc/init.d/${service_name}"],
+  }
+
+}
